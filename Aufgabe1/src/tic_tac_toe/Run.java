860b5eee6c3e02e1361e1b61bd5af4dba7fc7ef6
olia <olia@DESKTOP-3E3V380>
2017-03-27 18:27:18 +0200

    # WARNING: head commit changed in the meantime
    Initial Setup

--- Aufgabe1/src/tic_tac_toe/Run.java
+++ Aufgabe1/src/tic_tac_toe/Run.java
@@ -1,19 +1,19 @@
 package tic_tac_toe;
 
 /**
- *
+ * Runs the game.
+ * 
  * @author olga.bielkina
  *
  */
 public class Run {
 
-  public static void main(String[] args) {
+	public static void main(String[] args) {
 
-    Controller game = new Controller();
-    game.setCurrentPlayer("X");
-    game.play();
+		Controller game = new Controller();
+		game.setCurrentPlayer("X");
+		game.play();
 
-
-  }
+	}
 
 }
